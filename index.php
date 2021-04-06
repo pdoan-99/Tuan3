b5803b554c72fbd9a7633797837e82c15c99cff7
Phuong <doanphuong250120@gmail.com>
2021-04-06 08:57:50 +0700

    commit 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//bai1
+  $x =5
+echo("$x");
+
+//bai2
+// $x=5;
+//  function myTest(){
+//      echo "<p>Variable x inside function is :$x</p>";
+//  }
+//  myTest();
+//  echo "<p>Variable x inside function is :$x</p>";
+
+//so nguyen to
+?>
